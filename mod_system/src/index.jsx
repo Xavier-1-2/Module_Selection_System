d63c01ec33662fa2d5715d6e13f4d3c4c37771a8
Xavier-1-2 <157252481+Xavier-1-2@users.noreply.github.com>
2025-10-18 16:43:19 -0400

    Mod

--- mod_system/src/index.jsx
+++ mod_system/src/index.jsx
@@ -1,18 +1,36 @@
+import { use } from 'react'
 import './style.css'
 
 function Index(){
+    const [modules, setmodules] = useState([])
+
+    const HandleModules = () => {
+        setmodules([...modules, { name: `Module ${modules.length + 1}` }]);
+    };
+    
     return(
-        <body>
+        
+       < div>
+        
         <header className='main_title'>
             <h1>Module Selection System</h1>
         </header>
 
-        <div>
-            <form>
-                <input type="text" />
-            </form>
+        <nav className='Navi'>
+            <h4>Student</h4>
+            <h4>Admin</h4>
+        </nav>
+
+        <div className='SearchBar'>
+            <input type="text" placeholder='Search' />
         </div>
-         </body>
+        <br />
+        
+        <div className='main_module_sel_section'>
+            <div className='Module_Data'><p>Module Name</p> <button>Add</button></div>
+        </div>
+        </div>
+         
     );
 };
 
